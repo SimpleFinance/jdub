1731d89db879c43d49dc68a905c179939a2166d1
Coda Hale <coda.hale@gmail.com>
2011-06-14 14:03:36 -0700

    Extract out Connection.

--- src/main/scala/com/codahale/jdub/Database.scala
+++ src/main/scala/com/codahale/jdub/Database.scala
@@ -1,12 +1,10 @@
 package com.codahale.jdub
 
-import java.sql.{Types, PreparedStatement}
+import java.sql
 import javax.sql.DataSource
-import com.codahale.logula.Logging
 import com.yammer.metrics.Instrumented
 import org.apache.tomcat.dbcp.pool.impl.GenericObjectPool
 import org.apache.tomcat.dbcp.dbcp.{PoolingDataSource, PoolableConnectionFactory, DriverManagerConnectionFactory}
-import scala.annotation.tailrec
 
 object Database {
   /**
@@ -25,7 +23,7 @@ object Database {
               checkConnectionWhileIdle: Boolean = true,
               checkConnectionHealthWhenIdleForMS: Long = 10000,
               closeConnectionIfIdleForMS: Long = 1000L * 60L * 30L,
-              healthCheckQuery: String = prependComment(PingQuery, PingQuery.sql)) = {
+              healthCheckQuery: String = Connection.prependComment(PingQuery, PingQuery.sql)) = {
     val factory = new DriverManagerConnectionFactory(url, username, password)
     val pool = new GenericObjectPool(null)
     pool.setMaxWait(maxWaitForConnectionInMS)
@@ -43,15 +41,20 @@ object Database {
     new Database(new PoolingDataSource(pool), pool)
   }
 
-  private def prependComment(obj: Object, sql: String) =
-    "/* %s */ %s".format(obj.getClass.getSimpleName.replace("$", ""), sql)
+
 }
 
 /**
  * A set of pooled connections to a database.
  */
-class Database protected(source: DataSource, pool: GenericObjectPool) extends Instrumented with Logging {
-  import Database._
+class Database protected(source: DataSource, pool: GenericObjectPool)
+  extends Connection with Instrumented {
+
+  protected def openConnection() = source.getConnection
+
+  protected def closeConnection(conn: sql.Connection) {
+    conn.close()
+  }
 
   /**
    * Returns {@code true} if we can talk to the database.
@@ -59,93 +62,9 @@ class Database protected(source: DataSource, pool: GenericObjectPool) extends In
   def ping() = apply(PingQuery)
 
   /**
-   * Performs a query and returns the results.
-   */
-  def apply[A](query: RawQuery[A]): A = {
-    query.timer.time {
-      val connection = source.getConnection
-      try {
-        if (log.isDebugEnabled) {
-          log.debug("%s with %s", query.sql, query.values.mkString("(", ", ", ")"))
-        }
-        val stmt = connection.prepareStatement(prependComment(query, query.sql))
-        try {
-          prepare(stmt, query.values)
-          val results = stmt.executeQuery()
-          try {
-            query.handle(results)
-          } finally {
-            results.close()
-          }
-        } finally {
-          stmt.close()
-        }
-      } finally {
-        connection.close()
-      }
-    }
-  }
-
-  /**
-   * Performs a query and returns the results.
-   */
-  def query[A](query: RawQuery[A]): A = apply(query)
-
-  /**
-   * Executes an update, insert, delete, or DDL statement.
-   */
-  def execute(statement: Statement) = {
-    statement.timer.time {
-      val connection = source.getConnection
-      try {
-        if (log.isDebugEnabled) {
-          log.debug("%s with %s", statement.sql, statement.values.mkString("(", ", ", ")"))
-        }
-        val stmt = connection.prepareStatement(prependComment(statement, statement.sql))
-        try {
-          prepare(stmt, statement.values)
-          stmt.executeUpdate()
-        } finally {
-          stmt.close()
-        }
-      } finally {
-        connection.close()
-      }
-    }
-  }
-
-  /**
-   * Executes an update statement.
-   */
-  def update(statement: Statement) = execute(statement)
-
-  /**
-   * Executes an insert statement.
-   */
-  def insert(statement: Statement) = execute(statement)
-
-  /**
-   * Executes a delete statement.
-   */
-  def delete(statement: Statement) = execute(statement)
-
-  /**
    * Closes all connections to the database.
    */
   def close() {
     pool.close()
-  }
-
-  @tailrec
-  private def prepare(stmt: PreparedStatement, values: Seq[Any], index: Int = 1) {
-    if (!values.isEmpty) {
-      val v = values.head
-      if (v == null) {
-        stmt.setNull(index, Types.NULL)
-      } else {
-        stmt.setObject(index, v.asInstanceOf[AnyRef])
-      }
-      prepare(stmt, values.tail, index + 1)
-    }
   }
 }
