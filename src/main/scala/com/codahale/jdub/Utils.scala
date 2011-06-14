1303f9d29780b18e499f0ed61ebe060d69027ee6
Coda Hale <coda.hale@gmail.com>
2011-06-14 15:59:30 -0700

    Inline the Connection class.

---
+++ src/main/scala/com/codahale/jdub/Utils.scala
@@ -0,0 +1,22 @@
+package com.codahale.jdub
+
+import scala.annotation.tailrec
+import java.sql.{Types, PreparedStatement}
+
+object Utils {
+  private[jdub] def prependComment(obj: Object, sql: String) =
+    "/* %s */ %s".format(obj.getClass.getSimpleName.replace("$", ""), sql)
+
+  @tailrec
+  private[jdub] def prepare(stmt: PreparedStatement, values: Seq[Any], index: Int = 1) {
+    if (!values.isEmpty) {
+      val v = values.head
+      if (v == null) {
+        stmt.setNull(index, Types.NULL)
+      } else {
+        stmt.setObject(index, v.asInstanceOf[AnyRef])
+      }
+      prepare(stmt, values.tail, index + 1)
+    }
+  }
+}
