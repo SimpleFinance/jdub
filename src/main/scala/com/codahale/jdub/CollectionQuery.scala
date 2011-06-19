8e45c212d6b7bf0cbc89a57787d7904601887f80
Coda Hale <coda.hale@gmail.com>
2011-06-18 20:04:08 -0700

    A type fix for 2.8.1.
    I forgot GenTraversable was a 2.9 addition.

--- src/main/scala/com/codahale/jdub/CollectionQuery.scala
+++ src/main/scala/com/codahale/jdub/CollectionQuery.scala
@@ -1,13 +1,12 @@
 package com.codahale.jdub
 
-import scala.collection.GenTraversable
 import scala.collection.generic.GenericCompanion
 
 /**
  * A query class which maps each row of a result set to a value and aggregates
  * the values using a generic companion.
  */
-abstract class CollectionQuery[CC[A] <: GenTraversable[A], A](companion: GenericCompanion[CC]) extends Query[CC[A]] {
+abstract class CollectionQuery[CC[A] <: Traversable[A], A](companion: GenericCompanion[CC]) extends Query[CC[A]] {
   def map(row: Row): A
   
   final def reduce(results: Iterator[Row]): CC[A] = {
@@ -23,7 +22,7 @@ abstract class CollectionQuery[CC[A] <: GenTraversable[A], A](companion: Generic
  * A query class which flat-maps each row of a result set to a value and
  * collects the values using a generic companion.
  */
-abstract class FlatCollectionQuery[CC[A] <: GenTraversable[A], A](companion: GenericCompanion[CC]) extends Query[CC[A]] {
+abstract class FlatCollectionQuery[CC[A] <: Traversable[A], A](companion: GenericCompanion[CC]) extends Query[CC[A]] {
   def flatMap(row: Row): Option[A]
 
   final def reduce(results: Iterator[Row]) = {
