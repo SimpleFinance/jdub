405687b8686883ec44dd4b0b5e322209d0bed621
Jarred Ward <jarred@simple.com>
2014-02-07 11:46:35 -0800

    Add newTransactionScope method to support breaking out of current transactionScope.
    Also, add listeners to transaction object so you can fire code
    onCommit, onRollback, and onClose.

--- src/main/scala/com/simple/jdub/Transaction.scala
+++ src/main/scala/com/simple/jdub/Transaction.scala
@@ -1,25 +1,48 @@
 package com.simple.jdub
 
 import java.sql.Connection
+import scala.collection.mutable.ListBuffer
 
 class Transaction(connection: Connection) extends Queryable {
+  private[this] var rolledback = false
 
   /**
-   * Performs a query and returns the results.
-   */
+    * Performs a query and returns the results.
+    */
   override def apply[A](query: RawQuery[A]): A = apply(connection, query)
 
   /**
-   * Executes an update, insert, delete, or DDL statement.
-   */
+    * Executes an update, insert, delete, or DDL statement.
+    */
   def execute(statement: Statement) = execute(connection, statement)
 
   /**
-   * Roll back the transaction.
-   */
+    * Roll back the transaction.
+    */
   def rollback() {
+    logger.debug("Rolling back transaction")
     connection.rollback()
+    rolledback = true
+    onRollback.foreach(_())
+  }
+
+  private[jdub] def commit() {
+    if (!rolledback) {
+      logger.debug("Committing transaction")
+      connection.commit()
+      onCommit.foreach(_())
+    }
+  }
+
+  private[jdub] def close() {
+    logger.debug("Closing transaction")
+    connection.close()
+    onClose.foreach(_())
   }
 
   def transaction[A](f: Transaction => A): A = f(this)
+
+  var onCommit: ListBuffer[() => Unit] = ListBuffer.empty[() => Unit]
+  var onClose: ListBuffer[() => Unit] = ListBuffer.empty[() => Unit]
+  var onRollback: ListBuffer[() => Unit] = ListBuffer.empty[() => Unit]
 }
