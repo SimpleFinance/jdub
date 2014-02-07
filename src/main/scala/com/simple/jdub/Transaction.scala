d6c808a244e295454ac31637799b676e746a8f9d
Jarred Ward <jarred@simple.com>
2014-02-07 11:51:31 -0800

    Formatting

--- src/main/scala/com/simple/jdub/Transaction.scala
+++ src/main/scala/com/simple/jdub/Transaction.scala
@@ -7,18 +7,18 @@ class Transaction(connection: Connection) extends Queryable {
   private[this] var rolledback = false
 
   /**
-    * Performs a query and returns the results.
-    */
+   * Performs a query and returns the results.
+   */
   override def apply[A](query: RawQuery[A]): A = apply(connection, query)
 
   /**
-    * Executes an update, insert, delete, or DDL statement.
-    */
+   * Executes an update, insert, delete, or DDL statement.
+   */
   def execute(statement: Statement) = execute(connection, statement)
 
   /**
-    * Roll back the transaction.
-    */
+   * Roll back the transaction.
+   */
   def rollback() {
     logger.debug("Rolling back transaction")
     connection.rollback()
